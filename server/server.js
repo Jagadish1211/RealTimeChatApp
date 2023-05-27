21bcdfbed045382f63753b3b94ea98b8faca653c
Jagadish1211 <mrjagadish1211@gmail.com>
2023-05-27 13:17:24 +0530

    Commit

--- server/server.js
+++ server/server.js
@@ -1,23 +1,40 @@
 const express = require("express")
 const dotenv = require("dotenv");
+const http = require('http')
 const mongoose = require("mongoose");
+const {Server} = require('socket.io');
+
+
 
 const UserRoutes = require("./routes/user.js");
 const ContactRoutes = require("./routes/contact.js");
 const verifyToken = require("./middlewares/authJWT.js");
 const MessageRoutes = require("./routes/messages.js");
 
-
 const app = express();
 
 try {
-    mongoose.connect("mongodb+srv://mrjagadish1211:N7BwSKwDyACYK@cluster0.j1pcnwy.mongodb.net/?retryWrites=true&w=majority");
-    console.log("Connected to MongoDB");
+  mongoose.connect("mongodb+srv://mrjagadish1211:N7BwSKwDyACYK@cluster0.j1pcnwy.mongodb.net/?retryWrites=true&w=majority");
+  console.log("Connected to MongoDB");
+  const server =  http.createServer(app);
+  const io = new Server(server, {cors: {origin :"*"}});
+
+  
+// establishing socketIO connection
+io.on('connection', (socket) => {
+
+  // this gets triggered when client side sends message
+  socket.on('new message', (messageData, messageStatus) => {
+    // get info about the sender and the recipient 
+    console.log(messageData)
+  })
+})
 } catch(error) {
-    console.log(`${error} : Could not connect to MongoDB`)
+  console.log(`${error} : Could not connect to MongoDB`)
 }
 
-dotenv.config();
+
+
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -39,7 +56,7 @@ app.use("/app", UserRoutes);
 
 app.use("/app", ContactRoutes);
 
-app.use("/app", MessageRoutes)
+// app.use("/app", MessageRoutes)
 
 
 const PORT = process.env.PORT || 5000;
@@ -48,6 +65,9 @@ app.get("/", (req, res) => {
     res.status(200).send("Server is running");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`)
-});
+
+
+
+dotenv.config();
+
+
