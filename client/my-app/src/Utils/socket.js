420b0aaea2838c795f2d0f1989ccf3c9dfd56809
Jagadish1211 <mrjagadish1211@gmail.com>
2024-03-31 17:26:55 +0530

    Changed port number for socker io server connection

--- client/my-app/src/Utils/socket.js
+++ client/my-app/src/Utils/socket.js
@@ -1,6 +1,6 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:80/", { autoConnect: true, transports: ['websocket'] });
+const socket = io("http://localhost:3005", { autoConnect: true, transports: ['websocket'] });
 
 export default socket;
